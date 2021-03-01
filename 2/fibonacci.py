c031ccb53a798e63808d4d5b92ee0ebeed8b1bdc
Peter Čuřík Jr <91764@stuba.sk>
2021-03-02 00:18:42 +0100

    feat: achieve the same results with events

--- 2/fibonacci.py
+++ 2/fibonacci.py
@@ -1,4 +1,4 @@
-from fei.ppds import Thread, Mutex
+from fei.ppds import Thread, Mutex, Event
 from time import sleep
 from random import randint
 
@@ -7,25 +7,28 @@ class Shared:
     def __init__(self):
         self.fib_arr = [0,1]
         self.m = Mutex()
+        self.e = Event()
 
     def fibonacci(self, i):
         sleep(randint(1, 10) / 10)
 
         while True:
-            self.m.lock()
+            #self.m.lock()
             if i+2 == len(self.fib_arr):
                 break
-            self.m.unlock()
+            self.e.wait()
+            #self.m.unlock()
 
         self.fib_arr.append(self.fib_arr[i] + self.fib_arr[i + 1])
-        self.m.unlock()
+        #self.m.unlock()
+        self.e.signal()
+        self.e.clear()
         print(self.fib_arr)
 
 
 threads_num = 10
 obj = Shared()
 threads = list()
-
 for i in range(threads_num):
     t = Thread(obj.fibonacci, i)
     threads.append(t)
