0058595c4f6484d3c941a64f6746c9e3aac4f307
Peter Čuřík Jr <91764@stuba.sk>
2021-03-16 06:26:47 +0100

    feat: finish solution with 8 monitors and 3 sensors

--- 4/main.py
+++ 4/main.py
@@ -32,9 +32,9 @@ def init():
     ls_cidlo = Lightswitch()
     valid_data = Event()
 
-    for monitor_id in range(2):
+    for monitor_id in range(8):
         Thread(monitor, monitor_id, valid_data, turniket, ls_monitor, access_data)
-    for cidlo_id in range(11):
+    for cidlo_id in range(3):
         Thread(cidlo, cidlo_id, turniket, ls_cidlo, valid_data, access_data)
 
 
@@ -42,30 +42,40 @@ def monitor(monitor_id, valid_data, turniket, ls_monitor, access_data):
     valid_data.wait()
 
     while True:
-        sleep(.5)
         turniket.wait()
-        pocet_citajucich_monitorov = ls_monitor.lock(access_data)
         turniket.signal()
 
-        print(f'monit "{monitor_id:02d}": '
-              f'pocet_citajucich_monitorov={pocet_citajucich_monitorov:02d}')
+        pocet_citajucich_monitorov = ls_monitor.lock(access_data)
+        trvanie_zapisu = randint(40,50)/1000
 
+        print(f'monit "{monitor_id:02d}": '
+              f'pocet_citajucich_monitorov={pocet_citajucich_monitorov:02d} '
+              f'trvanie_citania={trvanie_zapisu:5.3f}')
+
+        sleep(trvanie_zapisu)
         ls_monitor.unlock(access_data)
 
 
 def cidlo(cidlo_id, turniket, ls_cidlo, valid_data, access_data):
     while True:
+        sleep(randint(50,60)/1000)
+
         turniket.wait()
+        pocet_zapisujucich_cidiel = ls_cidlo.lock(access_data)
+
+        if cidlo_id == 2:
+            trvanie_zapisu = randint(20,25)/1000
+        else:
+            trvanie_zapisu = randint(10,20)/1000
         turniket.signal()
 
-        pocet_zapisujucich_cidiel = ls_cidlo.lock(access_data)
-        trvanie_zapisu = randint(10,15)/1000
         print(f'cidlo "{cidlo_id:02d}": '
               f'pocet_zapisujucich_cidiel={pocet_zapisujucich_cidiel:02d}, '
               f'trvanie_zapisu={trvanie_zapisu:5.3f}')
         sleep(trvanie_zapisu)
-        valid_data.signal()
+
         ls_cidlo.unlock(access_data)
+        valid_data.signal()
 
 
 if __name__ == '__main__':
