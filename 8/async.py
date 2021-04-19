2162449972adfdd03eb992eff6fc9ba00b38d5b7
Peter Čuřík Jr <91764@stuba.sk>
2021-04-19 23:59:39 +0200

    fix: replace blocking network library with aiohttp

--- 8/async.py
+++ 8/async.py
@@ -1,21 +1,20 @@
-import json
 import asyncio
-import urllib.request as r
-from urllib.error import HTTPError
+from aiohttp import ClientSession
 
 
 async def get_data(city):
-    try:
-        data = r.urlopen('https://restcountries.eu/rest/v2/capital/' + city).read()
-        print_data(json.loads(data)[0], city)
-    except HTTPError:
-        pass
+    async with ClientSession() as s:
+        async with s.get('https://restcountries.eu/rest/v2/capital/' + city) as r:
+            print_data(await r.json(), city)
 
 
-def print_data(json_data, city):
+def print_data(data, city):
+    data = data[0]
     print('\n------------------------------')
     print('Here is the information about ' + city + ':')
-    print('Country:', json_data['name'])
+    print('Country:', data['name'])
+    print('Borders:', data['borders'])
+    print('Population:', data['population'])
     print('------------------------------')
 
 
